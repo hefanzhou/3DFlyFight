434b1486de85d8659b251c34b5ba67966cf2c1af
zhouhefan <276331025@qq.com>
2015-12-15 17:34:02 +0800

    1、删除ngui下无用资源 2、增加annoyEventManger模块

---
+++ FlyFight/Assets/Scripts/Event/AnonymousEvent.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AnonymousEvent
+{
+	/// <summary>
+	/// handler delegate used by AnonymousEventManager
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="sender"></param>
+	/// <param name="arg"></param>
+	public delegate void AnonymousEventHandler<T>(Object sender, T arg) where T : EventArgs;
+
+	/// <summary>
+	/// Provide anonymous event source.
+	/// The internal "event" keyword provide "named" event source. Listeners must can see the event variable.
+	/// While with this class, listeners only need known the event argument type.
+	/// Type of event argument serves as matching key.
+	/// </summary>
+	public class AnonymousEventManager
+	{
+		private static AnonymousEventManager m_instance = null;
+		public static AnonymousEventManager Instance
+		{
+			get {
+				m_instance = m_instance == null ? new AnonymousEventManager() : m_instance;
+				return m_instance;
+			}
+		}
+		/// <summary>
+		/// Raise event, all handler registered with argument type T will receive the event
+		/// </summary>
+		/// <typeparam name="T">Can not be EventArgs</typeparam>
+		/// <param name="sender"></param>
+		/// <param name="arg"></param>
+		public void RaiseEvent<T>(object sender, T arg) where T : EventArgs
+		{
+			CheckNullReference(arg, "arg");
+
+			if (typeof(T) == typeof(EventArgs))
+				throw new Exception("T can not be EventArgs");
+
+			HandlerChain<T> handlerChain = FindHandlerChain<T>();
+			if (handlerChain != null)
+				handlerChain.Invoke(sender, arg);
+		}
+
+		/// <summary>
+		/// Raise event, all handler registered with any type that is in arg's type's inherit branch will receive the event
+		/// The invokde sequence is from base type to inherited type
+		/// e.g ArgType3 extends ArgType2, ArgType2 extends ArgType1, Arg1 extends EventArgs. 
+		/// RaiseEventIncludingBase with ArgType3 equivalent to: RaiseEvent with ArgType1, ArgType2 and ArgType3
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="arg"></param>
+		public void RaiseEventIncludingBase(object sender, EventArgs arg)
+		{
+			CheckNullReference(arg, "arg");
+			if (arg.GetType() == typeof(EventArgs))
+				throw new Exception("typeof arg can not be EventArgs");
+
+			RaiseEventIncludingBaseWithHandlerType(sender, arg, arg.GetType());
+		}
+
+		private void RaiseEventIncludingBaseWithHandlerType(object sender, EventArgs arg, Type handlerArgType)
+		{
+			//recursively RaiseEvent for base type
+			Type handlerArgBaseType = handlerArgType.BaseType;
+			if (handlerArgBaseType != typeof(EventArgs))
+				RaiseEventIncludingBaseWithHandlerType(sender, arg, handlerArgBaseType);
+
+			if (!handlerArgType.IsAbstract)
+			{
+				HandlerChainBase handler = FindHandlerChainBase(handlerArgType);
+				if (handler != null)
+					handler.InvokeNonGeneric(sender, arg);
+			}
+		}
+
+		/// <summary>
+		/// Register and handler with argment type as T
+		/// </summary>
+		/// <typeparam name="T">Can not be EventArgs</typeparam>
+		/// <param name="f"></param>
+		public void AddHandler<T>(AnonymousEventHandler<T> handler) where T : EventArgs
+		{
+			CheckNullReference(handler, "handler");
+
+			if (typeof(T) == typeof(EventArgs))
+				throw new Exception("T can not be EventArgs");
+
+			HandlerChain<T> handlerChain = RequireHandlerChain<T>();
+			handlerChain.AddHandler(handler);
+		}
+
+		/// <summary>
+		/// Remove and handler with argment type as T
+		/// </summary>
+		/// <typeparam name="T">Can not be EventArgs</typeparam>
+		/// <param name="f"></param>
+		public void RemoveHandler<T>(AnonymousEventHandler<T> handler) where T : EventArgs
+		{
+			CheckNullReference(handler, "handler");
+
+			if (typeof(T) == typeof(EventArgs))
+				throw new Exception("T can not be EventArgs");
+
+			HandlerChain<T> handlerChain = FindHandlerChain<T>();
+			if (handlerChain != null)
+				handlerChain.RemoveHandler(handler);
+		}
+
+		/// <summary>
+		/// Clear handlers with argument type T
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		public void Clear<T>()
+		{
+			m_handlerChainMap.Remove(typeof(T));
+		}
+
+		/// <summary>
+		/// Clear all handlers
+		/// </summary>
+		public void ClearAll()
+		{
+			m_handlerChainMap.Clear();
+		}
+
+		private abstract class HandlerChainBase
+		{
+			public abstract void InvokeNonGeneric(Object sender, Object arg);
+		}
+
+		private class HandlerChain<T> : HandlerChainBase where T : EventArgs
+		{
+			public override void InvokeNonGeneric(Object sender, Object arg)
+			{
+				Invoke(sender, (T)arg);
+			}
+
+			public void Invoke(Object sender, T arg)
+			{
+
+				if (m_delegateChain != null)
+					m_delegateChain(sender, arg);
+			}
+
+			public void AddHandler(AnonymousEventHandler<T> handler)
+			{
+				m_delegateChain += handler;
+			}
+
+			public void RemoveHandler(AnonymousEventHandler<T> handler)
+			{
+				m_delegateChain -= handler;
+			}
+
+			AnonymousEventHandler<T> m_delegateChain = null;
+		}
+
+		private Dictionary<Type, HandlerChainBase> m_handlerChainMap = new Dictionary<Type, HandlerChainBase>();
+		private HandlerChainBase FindHandlerChainBase(Type type)
+		{
+			HandlerChainBase handlerChainBase;
+			if (m_handlerChainMap.TryGetValue(type, out handlerChainBase))
+			{
+				return handlerChainBase;
+			}
+			else
+			{
+				return null;
+			}
+		}
+		private HandlerChain<T> FindHandlerChain<T>() where T : EventArgs
+		{
+			HandlerChainBase handlerChainBase;
+			if (m_handlerChainMap.TryGetValue(typeof(T), out handlerChainBase))
+			{
+				return (HandlerChain<T>)handlerChainBase;
+			}
+			else
+			{
+				return null;
+			}
+		}
+		private HandlerChain<T> RequireHandlerChain<T>() where T : EventArgs
+		{
+			HandlerChainBase handlerChainBase;
+			if (m_handlerChainMap.TryGetValue(typeof(T), out handlerChainBase))
+			{
+				return (HandlerChain<T>)handlerChainBase;
+			}
+			else
+			{
+				HandlerChain<T> newChain = new HandlerChain<T>();
+				m_handlerChainMap.Add(typeof(T), newChain);
+				return newChain;
+			}
+		}
+		private static void CheckNullReference(Object reference, String name)
+		{
+			if (reference == null)
+				throw new NullReferenceException(name);
+		}
+	}
+}	//end namespace AnonymousEvent
--- FlyFight/Assets/Scripts/Test/TestNetWork.cs
+++ FlyFight/Assets/Scripts/Test/TestNetWork.cs
@@ -2,155 +2,162 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class PlayerInfo
+namespace Test
 {
-	public NetworkPlayer networkPlayer;
-	public string name;
-	public PlayerInfo(NetworkPlayer _networkPlayer, string _name = "")
+
+	public class PlayerInfo
 	{
-		networkPlayer = _networkPlayer;
-		name = _name;
-	}
-}
-public class TestNetWork : MonoBehaviour {
-
-	private bool isSever = false;
-	private Dictionary<NetworkPlayer, PlayerInfo> allPlayer = new Dictionary<NetworkPlayer, PlayerInfo>();
-	private string tipInf = "";
-	private string myName = "input your name";
-	void Awake()
-	{
-
-	}
-
-	void OnGUI() {
-
-		if (GUILayout.Button("Create Server"))
+		public NetworkPlayer networkPlayer;
+		public string name;
+		public PlayerInfo(NetworkPlayer _networkPlayer, string _name = "")
 		{
-			InitServer();
-			NetworkConnectionError err =  Network.InitializeServer(32, 53214, false);
-			if (err == NetworkConnectionError.NoError)
+			networkPlayer = _networkPlayer;
+			name = _name;
+		}
+	}
+	public class TestNetWork : MonoBehaviour
+	{
+
+		private bool isSever = false;
+		private Dictionary<NetworkPlayer, PlayerInfo> allPlayer = new Dictionary<NetworkPlayer, PlayerInfo>();
+		private string tipInf = "";
+		private string myName = "input your name";
+		void Awake()
+		{
+
+		}
+
+		void OnGUI()
+		{
+
+			if (GUILayout.Button("Create Server"))
 			{
-				MasterServer.RegisterHost("fps", "flyFight", "this is comment");
-				isSever = true;
+				InitServer();
+				NetworkConnectionError err = Network.InitializeServer(32, 53214, false);
+				if (err == NetworkConnectionError.NoError)
+				{
+					MasterServer.RegisterHost("fps", "flyFight", "this is comment");
+					isSever = true;
+				}
+				else
+				{
+					tipInf = "initialize server fail err:" + err.ToString();
+				}
 			}
-			else
+
+			if (GUILayout.Button("Connect to Server"))
 			{
-				tipInf = "initialize server fail err:" + err.ToString();
+				InitClient();
+				isSever = false;
+			}
+
+			if (isSever) ServerGUI();
+			else ClientGUI();
+		}
+
+		void InitServer()
+		{
+			MasterServer.ipAddress = "127.0.0.1";
+			MasterServer.port = 23466;
+
+		}
+
+		void ServerGUI()
+		{
+
+			if (GUILayout.Button("beginGame"))
+			{
+				networkView.RPC("InitiScence", RPCMode.AllBuffered);
+			}
+			if (GUILayout.Button("RPC"))
+			{
+				GameObject.Find("/Go1").GetComponent<TestPRC>().networkView.RPC("PrintSome", RPCMode.AllBuffered);
+			}
+			if (GUILayout.Button("clearRPC"))
+			{
+
+			}
+			tipInf = "initialize server success, connect player:";
+			foreach (var player in allPlayer) tipInf += "\n" + player.Value.name;
+			GUILayout.TextArea(tipInf);
+		}
+
+		void InitClient()
+		{
+			MasterServer.ipAddress = "127.0.0.1";
+			MasterServer.port = 23466;
+			MasterServer.RequestHostList("fps");
+		}
+
+		void ClientGUI()
+		{
+			HostData[] data = MasterServer.PollHostList();
+			foreach (HostData hostData in data)
+			{
+				GUILayout.BeginHorizontal();
+				string name = hostData.gameName + " " + hostData.connectedPlayers + " " + hostData.playerLimit;
+				GUILayout.Label(name);
+				GUILayout.Space(5);
+				string hostInfo = "";
+				foreach (string hostIp in hostData.ip)
+				{
+					hostInfo += hostIp + ":" + hostData.port + "\n";
+				}
+
+				GUILayout.Label(hostInfo);
+				GUILayout.Space(5);
+				GUILayout.FlexibleSpace();
+
+				if (GUILayout.Button("Connect"))
+				{
+					var netErr = Network.Connect(hostData);
+					Debug.Log(netErr);
+				}
+
+				myName = GUILayout.TextArea(myName);
+
+				GUILayout.EndHorizontal();
 			}
 		}
 
-		if (GUILayout.Button("Connect to Server"))
+		void OnPlayerConnected(NetworkPlayer player)
 		{
-			InitClient();
-			isSever = false;
+			Debug.Log("Player connected from " + player.ipAddress + ":" + player.port);
+			PlayerInfo playerInfo = new PlayerInfo(player);
+			allPlayer.Add(player, playerInfo);
 		}
 
-		if (isSever) ServerGUI();
-		else ClientGUI();
-    }
-
-	void InitServer()
-	{
-		MasterServer.ipAddress = "127.0.0.1";
-		MasterServer.port = 23466;  
-
-	}
-
-	void ServerGUI()
-	{
-
-		if (GUILayout.Button("beginGame"))
+		void OnConnectedToServer()
 		{
-			networkView.RPC("InitiScence", RPCMode.AllBuffered);			
+			Debug.Log("Connected to server");
+			networkView.RPC("SendMyInfo", RPCMode.Server, myName);
 		}
-		if (GUILayout.Button("RPC"))
+		void OnFailedToConnect(NetworkConnectionError error)
 		{
-			GameObject.Find("/Go1").GetComponent<TestPRC>().networkView.RPC("PrintSome", RPCMode.AllBuffered);
+			Debug.Log("Could not connect to server: " + error);
 		}
-		if (GUILayout.Button("clearRPC"))
+		[RPC]
+		void InitiScence()
 		{
-			
+			GameObject obj = Resources.Load("Cube") as GameObject;
+			GameObject go = Instantiate(obj, Vector3.zero, Quaternion.identity) as GameObject;
+
 		}
-		tipInf = "initialize server success, connect player:";
-		foreach (var player in allPlayer) tipInf += "\n" + player.Value.name;
-		GUILayout.TextArea(tipInf);
+
+		void OnPlayerDisconnected(NetworkPlayer player)
+		{
+			Debug.Log("Clean up after player " + player);
+			Network.RemoveRPCs(player);
+			Network.DestroyPlayerObjects(player);
+			allPlayer.Remove(player);
+		}
+
+		[RPC]
+		void SendMyInfo(string playerInfo, NetworkMessageInfo info)
+		{
+			allPlayer[info.sender].name = playerInfo;
+		}
+
 	}
 
-	void InitClient()
-	{
-		MasterServer.ipAddress = "127.0.0.1";
-		MasterServer.port = 23466;
-		MasterServer.RequestHostList("fps");  
-	}
-
-	void ClientGUI()
-	{
- 		 HostData[] data = MasterServer.PollHostList();  
-        foreach (HostData hostData in data)  
-        {  
-            GUILayout.BeginHorizontal();  
-            string name = hostData.gameName + " " + hostData.connectedPlayers + " " + hostData.playerLimit;  
-            GUILayout.Label(name);  
-            GUILayout.Space(5);  
-            string hostInfo = "";  
-            foreach (string hostIp in hostData.ip)  
-            {  
-                hostInfo += hostIp + ":" + hostData.port + "\n";  
-            }  
-  
-            GUILayout.Label(hostInfo);  
-            GUILayout.Space(5);  
-            GUILayout.FlexibleSpace();  
-  
-            if(GUILayout.Button("Connect"))  
-            {  
-               var netErr =  Network.Connect(hostData);
-			   Debug.Log(netErr);
-            }
-
-			myName = GUILayout.TextArea(myName);
-  
-            GUILayout.EndHorizontal();  
-        }  
-    }
-
-	void OnPlayerConnected(NetworkPlayer player)
-	{
-		Debug.Log("Player connected from " + player.ipAddress + ":" + player.port);
-		PlayerInfo playerInfo = new PlayerInfo(player);
-		allPlayer.Add(player, playerInfo);
-	}
-
-	void OnConnectedToServer()
-	{
-		Debug.Log("Connected to server");
-		networkView.RPC("SendMyInfo", RPCMode.Server, myName);
-	}
-	void OnFailedToConnect(NetworkConnectionError error)
-	{
-		Debug.Log("Could not connect to server: " + error);
-	}
-	[RPC]
-	void InitiScence()
-	{
-		GameObject obj = Resources.Load("Cube") as GameObject;
-		GameObject go = Instantiate(obj, Vector3.zero, Quaternion.identity) as GameObject;
-		
-	}
-
-	void OnPlayerDisconnected(NetworkPlayer player)
-	{
-		Debug.Log("Clean up after player " + player);
-		Network.RemoveRPCs(player);
-		Network.DestroyPlayerObjects(player);
-		allPlayer.Remove(player);
-	}
-
-	[RPC]
-	void SendMyInfo(string playerInfo, NetworkMessageInfo info)
-	{
-		allPlayer[info.sender].name = playerInfo;
-	}
-	
 }
