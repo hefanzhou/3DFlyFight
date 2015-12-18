b6347e9c0d1ce6b527d7fe46a0de7bcb57f6f564
zhouhefan <276331025@qq.com>
2015-12-18 12:26:59 +0800

    增加logOutputPanel 可在发布后查看log信息

--- FlyFight/Assets/Scripts/Managers/LogManager.cs
+++ FlyFight/Assets/Scripts/Managers/LogManager.cs
@@ -3,14 +3,51 @@ using System.Collections;
 
 public class LogManager : MonoBehaviour
 {
+	private UILabel		lable = null;
+	private UITextList	textList = null;
+	private UIButton	clearBtn = null;
+	private UIButton	toggleBtn = null;
+	private GameObject	panel = null;
 
-	// Use this for initialization
+	private string[] logStr = { "[FF4040]{0}[-]", "[FF4040]{0}[-]", "[EEC900]{0}[-]", "[A2CD5A]{0}[-]", "[FF4040]{0}[-]" };
+
+	void Awake()
+	{
+		DontDestroyOnLoad(transform.gameObject);
+		DontDestroyOnLoad(transform.parent.gameObject);
+	}
 	void Start () {
-	
+		InitProperty();
 	}
 	
-	// Update is called once per frame
-	void Update () {
+
+	void InitProperty()
+	{
+		lable		= transform.Find("panel/Label").GetComponent<UILabel>();
+		textList	= transform.Find("panel/TextList").GetComponent<UITextList>();
+		clearBtn	= transform.Find("panel/ClearBtn").GetComponent<UIButton>();
+		toggleBtn	= transform.Find("ToggleBtn").GetComponent<UIButton>();
+		panel		= transform.Find("panel").gameObject; 
+
+		UIEventListener.Get(clearBtn.gameObject).onClick += OnClearBtnClick;
+		UIEventListener.Get(toggleBtn.gameObject).onClick += OnToggleBtnClick;
+		Application.RegisterLogCallback(HandleLog);
 	
+	}
+
+	void HandleLog(string logString, string stackTrace, LogType type)
+	{
+		if (type.Equals(LogType.Error) || type.Equals(LogType.Exception) || type.Equals(LogType.Assert)) logString += "/n" + stackTrace;
+		textList.Add(string.Format(logStr[(int)type], logString));
+	}
+
+	void OnClearBtnClick(GameObject _)
+	{
+		textList.Clear();
+	}
+
+	void OnToggleBtnClick(GameObject _)
+	{
+		panel.SetActive(!panel.active);
 	}
 }
