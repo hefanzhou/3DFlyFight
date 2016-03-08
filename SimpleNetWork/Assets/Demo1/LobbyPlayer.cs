3a8c368f15b5f2a7d38481eb4d4e2adcc6b45c67
zhouhefan <276331025@qq.com>
2016-03-08 16:21:57 +0800

    edit match model

---
+++ SimpleNetWork/Assets/Demo1/LobbyPlayer.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+namespace Demo
+{
+    public class LobbyPlayer : NetworkLobbyPlayer
+    {
+        public Button joinBtn = null;
+        public InputField nameInput = null;
+
+        [SyncVar(hook="OnMyName")]
+        public string playerName = "";
+
+        public override void OnClientEnterLobby()
+        {//生成lobbyPlayer, 当进入一个客户端  （如果房间里有已有n个客户端，那么当本地生成这n个player时，每个对象上都会调用） 适合做初始化操作
+
+            Debug.Log("@@@@@OnClientEnterLobby" + isLocalPlayer);
+            base.OnClientEnterLobby();
+            transform.parent = UIManager.instance.lobbyPlayerRoot;
+            if (!isLocalPlayer)
+            {
+                SetupOtherPlayer();
+            }
+            //其他paly第一次同步过来时 不会触发playername的hook函数， 需手动调用hook函数
+            OnMyName(playerName);
+        }
+
+        public override void OnStartAuthority()
+        {//获得了管理权，这里可判断为自己
+            Debug.Log("@@@OnStartAuthority");
+            SetupLocalPlayer();
+        }
+
+        public override void OnClientReady(bool readyState)
+        {//玩家进入准备状态
+            SendReadyToBeginMessage();
+        }
+
+
+        public void OnReadyClicked()
+        {
+            SendReadyToBeginMessage();
+        }
+
+        void SetupLocalPlayer()
+        {
+            nameInput.interactable = true;
+            joinBtn.interactable = true;
+
+            joinBtn.onClick.AddListener(OnReadyClicked);
+            nameInput.onEndEdit.RemoveAllListeners();
+            nameInput.onEndEdit.AddListener(OnEditName);
+            if (playerName == "")
+                CmdNameChanged("Player" + (UIManager.instance.lobbyPlayerRoot.transform.childCount));
+        }
+
+        void SetupOtherPlayer()
+        {
+            nameInput.interactable = false;
+            joinBtn.interactable = false;
+        }
+        public void OnMyName(string newName)
+        {
+            Debug.Log("@@@@@OnMyName:" + playerName +"->"+newName );
+            playerName = newName;
+            nameInput.text = playerName;
+        }
+
+        //name change, server call
+        [Command]
+        public void CmdNameChanged(string name)
+        {
+            playerName = name;
+        }
+
+        //name change, local call
+        public void OnEditName(string str)
+        {
+            CmdNameChanged(str);
+        }
+
+        [ClientRpc]
+        public void RpcUpdateCountdown(int countdown)
+        {
+            Debug.Log("@@@@RpcUpdateCountdown" + countdown);
+            UIManager.instance.UpdateCooldownPanel(countdown);
+        }
+
+
+    }
+}
+
