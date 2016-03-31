118e63ae6d67833671f370e268f88485694a45d2
zhouhefan <276331025@qq.com>
2016-03-31 20:31:36 +0800

    网络模块

---
+++ FlyFight/Assets/ZHF/Scripts/Network/LobbyServerEntry.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+using UnityEngine.Networking.Types;
+
+    public class LobbyServerEntry : MonoBehaviour
+    {
+        public Text serverInfoText;
+        public Text slotInfo;
+        public Button joinButton;
+
+
+        private MatchDesc curMatch;
+
+
+        public void Populate(MatchDesc match)
+        {
+            curMatch = match;
+            serverInfoText.text = match.name;
+
+            slotInfo.text = match.currentSize.ToString() + "/" + match.maxSize.ToString(); ;
+
+            NetworkID networkID = match.networkId;
+
+            joinButton.onClick.RemoveAllListeners();
+            joinButton.onClick.AddListener(() => { GameLobbyManger.Instance.JoinMatch(networkID); });
+
+        }
+
+
+    }
