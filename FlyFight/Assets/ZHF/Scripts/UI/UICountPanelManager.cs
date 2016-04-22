292361dee0503b52b033742a83bc87194753f5c0
zhouhefan <276331025@qq.com>
2016-04-22 16:11:15 +0800

    添加结算界面

---
+++ FlyFight/Assets/ZHF/Scripts/UI/UICountPanelManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UICountPanelManager : MonoBehaviour {
+    
+    public GameObject itempPlayerConutPfb;
+    private Transform listRoot;
+    private static UICountPanelManager instance;
+
+    public static UICountPanelManager Instance
+    {
+        get { return UICountPanelManager.instance; }
+    }
+
+    void Awake()
+    {
+        instance = this;
+        listRoot = transform.Find("CountList");
+    }
+
+    public UIPlayerInfo AddPlayerInfo()
+    {
+        GameObject go = Instantiate(itempPlayerConutPfb) as GameObject;
+        go.transform.SetParent(listRoot, false);
+        return go.GetComponent<UIPlayerInfo>();
+    }
+
+}
---
+++ FlyFight/Assets/ZHF/Scripts/UI/UIPlayerCount.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIPlayerCount : MonoBehaviour {
+
+    public Color highLightColor;
+    private Image bgImage;
+    private Image headImage;
+    private Text nameText;
+    private Text kdText;
+
+    void Awake()
+    {
+        bgImage = transform.Find("BG").GetComponent<Image>();
+        headImage = transform.Find("HeadImage").GetComponent<Image>();
+        nameText = transform.Find("NameText").GetComponent<Text>();
+        kdText = transform.Find("KDText").GetComponent<Text>();
+    }
+	
+    public void SetHighLight()
+    {
+        bgImage.color = highLightColor;
+    }
+
+    public void SetName(string name)
+    {
+        nameText.text = name;
+    }
+
+    public void SetHeadImage(Sprite sprite)
+    {
+        headImage.sprite = sprite;
+    }
+
+    public void SetKdText(int kill, int died)
+    {
+        kdText.text = "Kill:" + kill + "  Died:" + died;
+    }
+}
